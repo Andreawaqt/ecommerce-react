48091161348ef28398700fe48de64016e652b143
Violeta Kunst <mmvioleta.kunst@gmail.com>
2024-07-01 18:43:55 +0200

    button sin css

--- src/components/ShopNow.jsx
+++ src/components/ShopNow.jsx
@@ -1,7 +1,14 @@
 function ShopNow() {
+    const handleClick = () => {
+        window.location.href = "/productos";
+    }
+    console.log ("Shop Now")
+
     return (
-        <>
-        </>
+        <button onClick={handleClick}>
+            Shop Now
+        </button>
     );
 }
-export default ShopNow;
+
+export default ShopNow; 
