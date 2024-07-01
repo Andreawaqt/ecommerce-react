cdc8eb4d0e09366902135ad6f9fff4026e86d0b5
Violeta Kunst <mmvioleta.kunst@gmail.com>
2024-07-01 18:58:56 +0200

    hero frse

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,7 @@ function Hero() {
         <div className="hero d-flex align-items-end justify-content-center">
             <span className="absolute">
             <h1 className="hero-title">Fashion</h1>
-            <p className="hero-section">Descubre la moda que te define. 
-                Estilo y calidad a un clic de distancia.</p>
+            <p className="hero-section">Descubre la moda que te define. <br /> Estilo y calidad a un clic de distancia.</p>
             </span>
             <i class="bi bi-arrow-down-short hero-arrow text-light"></i>
         </div>
