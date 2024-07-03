5df907976c31386ca42369e77f20e0bf4686a809
Dani Maya <mayathedev97@gmail.com>
2024-07-03 16:42:18 +0200

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import ShopNow from "./components/ShopNow.jsx";
 import Shop from "./components/Shop.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   const [edicion, setEdicion] = useState(false);
@@ -16,6 +17,7 @@ function App() {
         <ShopNow />
 
         <Shop/>
+        <About/>
       <Productos edicion={edicion}setEdicion={setEdicion}></Productos>
     </>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+
+
+function About(){
+
+
+    return(
+        <>
+        <div className="container" id="about">
+            <h1>ABOUT</h1>
+        </div>
+        </>
+    )
+}
+
+export default About;
